fe88fe22185edd74ec5381413894a9fde7ff49d7
sourajitm <sourajit.mohantyTA@gmail.com>
2019-11-18 19:42:51 +0530

    First Commit....

---
+++ src/main/java/com/test/aws/SpingBootAwsApplication.java
@@ -0,0 +1,13 @@
+package com.test.aws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpingBootAwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpingBootAwsApplication.class, args);
+	}
+
+}
